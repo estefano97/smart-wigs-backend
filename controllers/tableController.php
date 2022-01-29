d6a68c18ae20e1fd83ee7ad45e56b4b8df970f63
estefano97 <kaliestefano@gmail.com>
2022-01-29 17:36:31 -0500

    First Version

--- controllers/tableController.php
+++ controllers/tableController.php
@@ -7,7 +7,8 @@ class tableController {
 
     public function getTable() {
         $controller = new conexion();
-        $getData = $controller->getData("SELECT * FROM users_st");
+        $getData = $controller->getData("SELECT name, points_primary, points_secondary FROM users_st ORDER BY points_primary DESC,
+        points_secondary ASC;");
 
         return $getData;
     }
--- table.php
+++ table.php
@@ -1,6 +1,4 @@
 <?php
-
-<?php
 header("Content-Type: application/json");
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
@@ -9,12 +7,14 @@ header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-include_once("./controllers/winController.php");
+include_once("./controllers/tableController.php");
 
 switch($_SERVER['REQUEST_METHOD']) {
     
     case 'GET':
+        $getData = new tableController();
         
+        echo $getData->getTable();
     break;
 
     default:
